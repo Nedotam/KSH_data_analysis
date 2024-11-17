234e4c498786be475cbc919097368d1f55b442c0
NedoTamas <nedo5435@gmail.com>
2024-11-17 12:41:08 +0100

    .

--- src/analysis.py
+++ src/analysis.py
@@ -1,15 +1,18 @@
+import sys
+print(sys.executable)  # This should point to the virtual environment python
+print(sys.path)        # This should include your virtual environment's site-packages
+
 import pandas as pd
 import numpy as np
 import scipy
 import scipy.stats as stats
 import seaborn as sns
 import matplotlib.pyplot as plt
-from src.utils import utils
+from src.utils import read_csv_file, utils
+
 
 path = '../data/raw/'
 file = 'dummy.csv'
-df=utils.read_csv_file()
-if df is not None:
-    print(df)
-if df is None:
-    print(':(')
+df=read_csv_file(path, file,'latin-1',';',)
+print(df)
+print('geci')
